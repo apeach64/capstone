95b628480ca8b11a27516c2ac7bfd1960679af25
Ubuntu <ubuntu@ip-172-31-92-214.ec2.internal>
2022-02-14 07:38:58 +0000

    js remark & aws address change

--- sensor/src/App.js
+++ sensor/src/App.js
@@ -8,8 +8,8 @@ function App() {
   const [commandTextToSend, setCommnadTextToSend] = useState("");
   useEffect(()=>{
     const loop = setInterval(()=>{
-      axios.get('http://ec2-3-83-65-82.compute-1.amazonaws.com:8080/api/sensor').then((data) => {
-	#axios로 get을 하는 경우 불러오는 데이터를 자동으로 json형태로 변환하여 얻어 JSON.parse를 사용하여 변환 X
+      axios.get('http://ec2-3-84-249-77.compute-1.amazonaws.com:8080/api/sensor').then((data) => {
+	//axios로 get을 하는 경우 불러오는 데이터를 자동으로 json형태로 변환하여 얻어 JSON.parse를 사용하여 변환 X
 	const json_data = data.data;
 	console.log(json_data);
         setSensorText(json_data.data);
@@ -27,7 +27,7 @@ function App() {
       time: Date.now()
     };
     console.log(toSend);
-    axios.post('http://ec2-3-83-65-82.compute-1.amazonaws.com:8080/api/command',JSON.stringify(toSend), {
+    axios.post('http://ec2-3-84-249-77.compute-1.amazonaws.com:8080/api/command',JSON.stringify(toSend), {
       headers:{
         'Content-Type': 'application/json'
       }
